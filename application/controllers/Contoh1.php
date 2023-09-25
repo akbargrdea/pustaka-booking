7a552d26f3a54ac732b2bb711f135920c6441b4f
akbargrdea <akbrgrd72@gmail.com>
2023-09-25 16:31:38 +0700

    Tambah data

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Moch. Akbar Garudea S.
+            Saya tinggal di daerah Maguwoharjo
+            Olah raga yang saya sukai adalah
+            Bersepeda";
+    }
+}
