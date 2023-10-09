05df1ab4fe72beb29a8ed12cca58da545f9a5db2
akbargrdea <akbrgrd72@gmail.com>
2023-10-09 16:19:07 +0700

    Penambahan Tugas Pertemuan 4 Membuat Template Sederhana

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -4,7 +4,7 @@ class Matakuliah extends CI_Controller
     public function index()
 
     {
-        $this->load->view('vew-form-matakuliah');
+        $this->load->view('view-form-matakuliah');
     }
 
     public function cetak()
